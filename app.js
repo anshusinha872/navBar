035f93d019624b9035d807d09c46a0da71e7e921
anshusinha872 <anshusinha872@gmail.com>
2022-07-02 17:29:53 +0530

    final

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const btn = document.getElementById('btn');
-const links = document.querySelectorAll('#link');
-links.forEach(function (e) {
-    // console.log(e);
-    btn.addEventListener('click', function () {
-        if (e.classList.contains('temp')) {
-            e.classList.remove('temp');
-        }
-        else {
-            e.classList.add('temp');
-        }
-    })
-})
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+    // console.log(links.classList);
+    // console.log(links.classList.contains("random"));
+    // console.log(links.classList.contains("links"));
+    // if (links.classList.contains("show-links")) {
+    //   links.classList.remove("show-links");
+    // } else {
+    //   links.classList.add("show-links");
+    // }
+    links.classList.toggle("show-links");
+});
